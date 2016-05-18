e82ba71ec713d87ba9d61867545a78c7462b7cda
agupta31 <akash2989@gmail.com>
2016-05-18 13:51:23 -0400

    updated

--- module patterns/greet/greet5.js
+++ module patterns/greet/greet5.js
@@ -1,4 +1,5 @@
-//revealing module pattern
+//revealing module pattern:
+//exposing only the proerpties and methods you want via a returned object
 var greeting="hello from revealing module pattern";
 
 function greet(){
